96bbc46e53c3fbc275018ba7a97505c634a51fab
RnD0mN3m <ktq2390306@stu.o-hara.ac.jp>
2024-01-22 14:35:01 +0900

    初期状態

--- java11/java11_7.java
+++ java11/java11_7.java
@@ -2,11 +2,11 @@ package test.java11;
 
 class Student {
     // クラス変数としての 名前 と 番数 と 国語 と 数学 と 英語
-    static String name;
-    static String number;
-    static double jap;
-    static double math;
-    static double eng;
+    String name;
+    String number;
+    double jap;
+    double math;
+    double eng;
 
     // コンストラクタ
     public Student(String name, String number, double jap, double math, double eng) {
@@ -31,12 +31,12 @@ class Student {
     }
 
     // 合計値を求めるメソッド
-    static double Total() {
+    double Total() {
         return jap+math+eng;
     }
 
     // 平均値を求めるメソッド
-    static double Average() {
+    double Average() {
         return (jap+math+eng)/3;
     }
 }
@@ -44,21 +44,21 @@ class Student {
 class java11_7 {
     public static void main(String[] args) {        
         System.out.println("test");
-        Student eisann = new Student("A さん","001",89,65,88);
+        Student eisann = new Student ("A さん","001",89,65,88);
         Student biisann = new Student("B さん","002",80,95,64);
-        // Student siisann = new Student("C さん","003",70,80,98);
-        System.out.println(eisann.getName());
+        Student siisann = new Student("C さん","003",70,80,98);
+        // System.out.println(eisann.getName());
         // System.out.println(biisann.getName());
         // System.out.println(siisann.getName());
         
         
         // // 平均値を求める
-        // double aaa = eisann.Average();
-        // double bbb = biisann.Average();
-        // double ccc = siisann.Average();
+        double aaa = eisann.Average();
+        double bbb = biisann.Average();
+        double ccc = siisann.Average();
         // // 結果を表示
-        // System.out.printf(eisann.getNumber() +" "+ eisann.getName() + "平均点%.2f\n",aaa);
-        // System.out.printf(biisann.getNumber() +" "+ biisann.getName() + "平均点%.2f\n",bbb);
-        // System.out.printf(siisann.getNumber() +" "+ siisann.getName() + "平均点%.2f\n",ccc);
+        System.out.printf(eisann.getNumber() +"番　"+ eisann.getName() + "　平均点%.2f\n",aaa);
+        System.out.printf(biisann.getNumber() +"番　"+ biisann.getName() + "　平均点%.2f\n",bbb);
+        System.out.printf(siisann.getNumber() +"番　"+ siisann.getName() + "　平均点%.2f\n",ccc);
     }
 }
