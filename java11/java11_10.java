d70b90bbe25114c380dbc8e933dfeaf7af949b72
RnD0mN3m <ktq2390306@stu.o-hara.ac.jp>
2024-01-23 14:56:49 +0900

    初期状態

--- java11/java11_10.java
+++ java11/java11_10.java
@@ -1,27 +1,27 @@
 package test.java11;
 
 class BookInfo {
-    private static int counter = 0;
-    private String book_title;
-    private String writer_name;
-    private String publication_year;
-    private int id;
+    static int counter = 0;
+    String book_title;
+    String writer_name;
+    String publication_year;
+    int id;
     public BookInfo(String b, String w, String p) {
         book_title = b;
         writer_name = w;
         publication_year = p;
         id = ++counter;
     }
-    public String getBook_Title() {
+    String getBook_Title() {
         return book_title;
     }
-    private String getWriter_Name() {
+    String getWriter_Name() {
         return writer_name;
     }
-    private String getPublication_Year() {
+    String getPublication_Year() {
         return publication_year;
     }
-    public int getId() {
+    int getId() {
         return id;
     }
 }
@@ -68,7 +68,7 @@ public class java11_10 {
         System.out.println("書籍番号：" + book4.getId());
 
         // 吾輩は猫である
-        System.out.println(" 書　　名：" + book5.getBook_Title());
+        System.out.println("書　　名：" + book5.getBook_Title());
         System.out.println("著 者 名：" + book5.getWriter_Name());
         System.out.println("出 版 年：" + book5.getPublication_Year());
         System.out.println("書籍番号：" + book5.getId());
@@ -79,3 +79,45 @@ public class java11_10 {
 // 3 
 // 4 
 //5
+class Person {
+    private static int counter = 0;
+    private int id;
+    
+    public int getId() {
+        return id;
+    }
+    protected String a, b, c; //, z
+    Person() { this.a = null; id = ++counter;}
+    Person(String x, String y) {this.a = a; this.b = b; this.c = c; }
+    // Person(String z) {this.z = z; }
+    void introduce() {System.out.println("書　　名：" + a); System.out.println("著 者 名：" + b); System.out.println("出 版 年：" + c); }
+}
+class Teacher extends Person {
+    String x, y; //, z
+    Teacher(String a1, String b1, String c1) {super.a = a1; super.b = b1; super.c = c1; }
+    void introduce() { super.introduce(); }
+}
+class java12_2 {
+    public static void main(String[] args) {
+        Teacher a = new Teacher("三国志", "吉川英治", "1989");
+        a.introduce();
+        System.out.print("書籍番号：" + a.getId());
+        System.out.println();
+        Teacher b = new Teacher("楊令伝","北方謙三","2011");
+        b.introduce();
+        System.out.print("書籍番号：" + b.getId());
+        System.out.println();
+        Teacher c = new Teacher("竜馬がゆく","司馬遼太郎","1998");
+        c.introduce();
+        System.out.print("書籍番号：" + c.getId());
+        System.out.println();
+        Teacher d = new Teacher("竜馬がゆく","司馬遼太郎","1998");
+        d.introduce();
+        System.out.print("書籍番号：" + d.getId());
+        System.out.println();
+        Teacher e = new Teacher("吾輩は猫である","夏目漱石","1905");
+        e.introduce();
+        System.out.print("書籍番号：" + e.getId());
+        System.out.println();
+    }
+}
